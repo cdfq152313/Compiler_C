e167b9fc8512e8d9477907f1b02386b73b74953d
denny <cdfq152313@gmail.com>
2015-04-23 18:30:55 +0800

    ParserGenerator

--- Makefile
+++ Makefile
@@ -1,35 +1,9 @@
 TMCODE = code.tm
-EDIR = ./exec/
-ODIR = ./output/
 
 #exec file
-${EDIR}compiler:${EDIR}main.o ${EDIR}Scanner.o ${EDIR}Parser.o ${EDIR}ParserGenerator.o ${EDIR}CodeGenerator.o ${EDIR}MCodeGenerator.o ${EDIR}Struct.o
-	g++ -o ${EDIR}compiler ${EDIR}main.o ${EDIR}Scanner.o ${EDIR}Parser.o ${EDIR}ParserGenerator.o ${EDIR}CodeGenerator.o ${EDIR}MCodeGenerator.o ${EDIR}Struct.o
-${EDIR}main.o:main.cpp
-	g++ -c main.cpp -o ${EDIR}main.o
-${EDIR}Scanner.o:Scanner.h Scanner.cpp
-	g++ -c Scanner.cpp -o ${EDIR}Scanner.o
-${EDIR}Parser.o:Parser.h Parser.cpp 
-	g++ -c Parser.cpp -o ${EDIR}Parser.o
-${EDIR}ParserGenerator.o:ParserGenerator.h ParserGenerator.cpp 
-	g++ -c ParserGenerator.cpp -o ${EDIR}ParserGenerator.o
-${EDIR}CodeGenerator.o:CodeGenerator.h CodeGenerator.cpp
-	g++ -c CodeGenerator.cpp -o ${EDIR}CodeGenerator.o
-${EDIR}MCodeGenerator.o:MCodeGenerator.h MCodeGenerator.cpp
-	g++ -c MCodeGenerator.cpp -o ${EDIR}MCodeGenerator.o
-${EDIR}Struct.o:Struct.h Struct.cpp
-	g++ -c Struct.cpp -o ${EDIR}Struct.o
-
-#tiny machine
-${ODIR}TM:Tiny_Machine.c
-	gcc Tiny_Machine.c -o ${ODIR}TM
-
-#exec
-exec:${ODIR}TM ${ODIR}${TMCODE} ${EDIR}compiler
-	cd ${ODIR}; ./TM ${TMCODE};
-${ODIR}${TMCODE}: ${EDIR}compiler ./input/grammar.txt ./input/main.c
-	${EDIR}compiler
+compiler:main.o ParserGenerator.o Struct.o
+	g++ -o compiler main.o ParserGenerator.o Struct.o
 
 clean:
-	rm -f ${EDIR}*
-	rm -f ${ODIR}*
+	rm -f  *.o
+	rm -f  compiler
--- main.cpp
+++ main.cpp
@@ -1,62 +1,16 @@
 #include <iostream>
 #include <string>
 #include "ParserGenerator.h"
-#include "Parser.h"
-#include "Scanner.h"
-#include "CodeGenerator.h"
-#include "MCodeGenerator.h"
 using namespace std;
-
 
 int main()
 {
-    Scanner scanner;
+    bool checkpg;
     ParserGenerator pg;
-    Parser pa;
-    CodeGenerator cg;
-    MCodeGenerator mcg;
-    bool checksc = false;
-    bool checkpg = false;
-    bool checkpa = false;
-/*
-    //scanner
-
-
-    scanner.open("./input/main.c");
-    checksc = scanner.start("./output/token.txt");
-    if(checksc){
-        scanner.PrinttoParser("./output/ParserToken.txt");
-    }
-*/
-    //ParserGenerator
-    pg.ReadGrammar("./input/grammar.txt");
-    checkpg = pg.ConstructNullFirstFollow("./output/set.txt");
-    checkpg = pg.GenerateLLTable("./output/LLtable.txt");
-
-    if(checkpg && checksc){
-        pa.ReadLLTable("./output/LLtable.txt");
-        pa.ReadToken("./output/ParserToken.txt");
-        checkpa = pa.LLParser();
-        if(checkpa){
-            pa.PrintTree("./output/tree.txt");
-        }
-    }
-/*
-    //intermediate code
-    cg.ReadParsingtree("./output/tree.txt");
-    cg.CreateSymbolTable("./output/symbol.txt");
-    cg.CreateSymbolTable2("./output/symbol2.txt");
-    cg.SemanticRule("./output/quadruples.txt");
-
-    //machine code
-    mcg.ReadSymbolTable("./output/symbol2.txt");
-    mcg.ReadIntermediateCode("./output/quadruples.txt");
-    mcg.MachineCodeGenerate("./output/code.tm");
-
-    //Parser
-    cout << "press ENTER to exit" << endl;
-    getchar();
-*/
+    pg.ReadGrammar("grammar.txt");
+    checkpg = pg.ConstructNullFirstFollow("set.txt");
+    checkpg = pg.GenerateLLTable("LLtable.txt");
+    
     return 0;
 }
 
